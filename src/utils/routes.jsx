72ff18cd9a817eb5b25561ef0b24288db2b6a775
Isutomu <isutomu@gmail.com>
2025-01-27 10:33:33 -0300

    Adds Github Actions

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -7,24 +7,27 @@ import CreatePost from "../components/CreatePost/CreatePost";
 import UpdatePost from "../components/UpdatePost/UpdatePost";
 import PostsCardsWrapper from "../components/PostsCardsWrapper/PostsCardsWrapper";
 
-const routes = new createBrowserRouter([
-  { index: true, element: <LogIn /> },
-  { path: "/logIn", element: <LogIn /> },
-  {
-    element: <ProtectedRoute />,
-    children: [
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
-        children: [
-          { index: true, element: <DashboardHome /> },
-          { path: "/dashboard/createPost", element: <CreatePost /> },
-          { path: "/dashboard/updatePost", element: <PostsCardsWrapper /> },
-          { path: "/dashboard/updatePost/:postId", element: <UpdatePost /> },
-        ],
-      },
-    ],
-  },
-]);
+const routes = new createBrowserRouter(
+  [
+    { index: true, element: <LogIn /> },
+    { path: "/logIn", element: <LogIn /> },
+    {
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+          children: [
+            { index: true, element: <DashboardHome /> },
+            { path: "/dashboard/createPost", element: <CreatePost /> },
+            { path: "/dashboard/updatePost", element: <PostsCardsWrapper /> },
+            { path: "/dashboard/updatePost/:postId", element: <UpdatePost /> },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: "/blog_api_admin-TOP/" }
+);
 
 export default routes;
